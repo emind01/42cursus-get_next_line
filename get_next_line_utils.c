2a4835c935ce5efbe30e52fa32a99daf435087e1
emind01 <emin.drn@icloud.com>
2024-12-04 11:03:33 +0300

    re

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,45 +1,48 @@
-size_t	ft_strlen(const char *s)
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *s)
 {
 	size_t	len;
 
 	len = 0;
+	if (!s)
+		return (0);
 	while (s[len] != '\0')
 		len++;
 	return (len);
 }
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strchr(char *s, int c)
 {
-	while (*s && (char)c != *s)
-		s++;
-	if ((char)c == *s)
-		return ((char *)s);
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	if (c == '\0')
+		return ((char *)&s[ft_strlen(s)]);
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char) c)
+			return ((char *)&s[i]);
+		i++;
+	}
 	return (0);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2, size_t s1_len, size_t s2_len)
 {
-	size_t	i;
-	size_t	j;
-	size_t	len;
-	char	*fstr;
+    char *str;
+    size_t i = 0;
+    size_t j = 0;
 
-	len = ft_strlen(s1) + ft_strlen(s2) + 1;
-	fstr = malloc(len);
-	if (!fstr)
-		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		fstr[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j])
-	{
-		fstr[i + j] = s2[j];
-		j++;
-	}
-	fstr[i + j] = '\0';
-	return (fstr);
+    str = malloc(s1_len + s2_len + 1);
+    if (!str)
+        return (NULL);
+    while (i < s1_len)
+        str[i++] = s1[i];
+    while (j < s2_len)
+        str[i++] = s2[j++];
+    str[i] = '\0';
+    return (str);
 }
