fedaf6d19a40f28e27e4609aa1fb514e59fbab47
emind01 <emin.drn@icloud.com>
2024-12-04 11:10:35 +0300

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -33,26 +33,27 @@ char	*ft_strchr(char *s, int c)
 char	*ft_strjoin(char *s1, char *s2)
 {
 	size_t	i;
-	size_t	j;
-	size_t	len;
-	char	*fstr;
+	size_t	c;
+	char	*str;
 
-	len = ft_strlen(s1) + ft_strlen(s2) + 1;
-	fstr = malloc(len);
-	if (!fstr)
+	if (!s1)
+	{
+		s1 = (char *)malloc(1 * sizeof(char));
+		s1[0] = '\0';
+	}
+	if (!s1 || !s2)
 		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		fstr[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j])
-	{
-		fstr[i + j] = s2[j];
-		j++;
-	}
-	fstr[i + j] = '\0';
-	return (fstr);
+	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	i = -1;
+	c = 0;
+	if (s1)
+		while (s1[++i] != '\0')
+			str[i] = s1[i];
+	while (s2[c] != '\0')
+		str[i++] = s2[c++];
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (str);
 }
